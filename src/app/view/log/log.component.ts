9b5712a71f32e9b631629faebb7b688f18578237
Trevor Weir <trevorhweir@gmail.com>
2021-03-24 17:48:22 -0400

    stable added log v1.0

--- src/app/view/log/log.component.ts
+++ src/app/view/log/log.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
+//service
+import { GlobalService } from '../../service/global.service';
+import { WebsocketService } from '../../service/websocket.service';
 @Component({
-  selector: 'app-log',
-  templateUrl: './log.component.html',
-  styleUrls: ['./log.component.css']
+    selector: 'app-log',
+    templateUrl: './log.component.html',
+    styleUrls: ['./log.component.css']
 })
 export class LogComponent implements OnInit {
 
-  constructor() { }
+    logData: Array<any> = [];
 
-  ngOnInit(): void {
-  }
+    orderOriginal = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+        return 0
+    }
+    constructor(
+        public globalVar: GlobalService,
+        private ws: WebsocketService,
+    ) { }
+
+    isArray(obj): boolean {
+        return obj instanceof Array;
+    }
+
+    getLogData(tableType: string): void {
+        console.log("clicked get log button")
+        this.ws.socket.emit('log', JSON.stringify({
+            tableType: tableType
+        }))
+    }
+
+    ngOnInit(): void {
+        this.getLogData("master")
+        this.ws.socket.on('log', (data) => {
+            data = JSON.parse(data);
+            this.logData = data.tableData
+        })
+    }
 
 }
